c90913ef67e72a3dfdd2cc8036dab31a7f87ed72
sanjiv510 <sanjiv510@gmail.com>
2016-01-22 15:04:04 -0800

    added dot product and angle

--- vector.py
+++ vector.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python -tt
 
-import math as math
+from math import sqrt, acos, degrees
 
 class Vector(object):
     def __init__(self, coordinates):
@@ -16,22 +16,56 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
-
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+    
+    def orthogonal(self, v):
+        return self.dot_product(v) == 0
 
-    def times(self, c):
+    def parallel(self, v):
+         self_normal = self.normalized()
+         v_normal = v.normalized()
+         return self_normal == v_normal or self_normal == v.times_scalar(-1)
+
+    def dot_product(self,v):
+        new_coordinates = [x*y for x, y in zip(self.coordinates, v.coordinates) ]
+        return sum(new_coordinates)
+    
+    def angle_in_rad(self,v):
+            dot_product = self.dot_product(v)
+            magnitude_product = self.magnitude()*v.magnitude()
+            try:
+                return acos(dot_product/magnitude_product)
+            except ZeroDivisionError:
+                raise 'One of the magnitude is zero, to get angle both vector magnitude should be greter than zero'
+    
+    def angle_in_deg(self, v):
+        rad = self.angle_in_rad(v)
+        return degrees(rad)
+
+    def plus(self, v):
+        new_coordinates = [x+y for x, y in zip(self.coordinates, v.coordinates) ]
+        return Vector(new_coordinates)
+
+    def minus(self, v):
+        new_coordinates = [x-y for x, y in zip(self.coordinates, v.coordinates) ]
+        return Vector(new_coordinates)
+
+
+    def times_scalar(self, c):
         new_coordinates = [x*c for x in self.coordinates]
         return Vector(new_coordinates)
 
     def magnitude(self):
-        side_sqrs = [x*x for x in self.coordinates]
-        return math.sqrt(sum(side_sqrs))
+        coordinates_squared = [x**2 for x in self.coordinates]
+        return sqrt(sum(coordinates_squared))
 
-    def direction(self):
-        mag = self.magnitude()
-        return self.times(1/mag)
+    def normalized(self):
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(1./magnitude)
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize zero vector')
