055a106f50dd72299947691d1c18b17b1a1f7f64
sanjiv510 <sanjiv510@gmail.com>
2016-01-22 17:08:24 -0800

    added Decimal and tolerance

--- vector.py
+++ vector.py
@@ -1,13 +1,16 @@
 #! /usr/bin/python -tt
 
-from math import sqrt, acos, degrees
+from math import sqrt, acos, degrees, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
 
 class Vector(object):
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -22,13 +25,21 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
     
-    def orthogonal(self, v):
-        return self.dot_product(v) == 0
+    def is_orthogonal_to(self, v, tolerance=1e-10):
+        return abs(self.dot_product(v))< tolerance
 
-    def parallel(self, v):
-         self_normal = self.normalized()
-         v_normal = v.normalized()
-         return self_normal == v_normal or self_normal == v.times_scalar(-1)
+    def is_parallel_to(self, v):
+        return ( self.is_zero() or
+                v.is_zero() or
+                self.angle_in_rad(v) == 0 or
+                self.angle_in_rad(v) == pi)
+    # def is_parallel_to(self, v):
+    #      self_normal = self.normalized()
+    #      v_normal = v.normalized()
+    #      return self_normal == v_normal or self_normal == v.times_scalar(-1)
+
+    def is_zero(self, tolerance=1e-10):
+        return self.magnitude() < tolerance
 
     def dot_product(self,v):
         new_coordinates = [x*y for x, y in zip(self.coordinates, v.coordinates) ]
@@ -61,11 +72,11 @@ class Vector(object):
 
     def magnitude(self):
         coordinates_squared = [x**2 for x in self.coordinates]
-        return sqrt(sum(coordinates_squared))
+        return Decimal(sqrt(sum(coordinates_squared)))
 
     def normalized(self):
         try:
             magnitude = self.magnitude()
-            return self.times_scalar(1./magnitude)
+            return self.times_scalar(Decimal('1.0')/magnitude)
         except ZeroDivisionError:
             raise Exception('Cannot normalize zero vector')
